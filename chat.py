f15134df871983c52531d2bb6699cf4c8b9f2f67
Rafa <rafavazquez229@gmail.com>
2020-09-22 22:52:21 -0700

    first

---
+++ chat.py
@@ -0,0 +1,41 @@
+# This will give you access to the random module or library.
+# choice() will randomly return an element in a list.
+# Read more: https://pynative.com/python-random-choice/
+
+from random import choice
+
+#combine functions and conditionals to get a response from the bot
+def get_day_bot_response(user_response):
+
+#add some bot responses to this list
+#bot_response_happy = ["omg! great!", "Keep smiling!", "I love to see you happy!"]
+bot_response_Monday = ["SPD class with the bois", "Sleeping in with the bois"]
+
+    if user_response == "Monday":
+        return choice(bot_response_Monday)
+
+    elif user_response == "Tuesday":
+        return choice(bot_response_Tuesday)
+
+    else:
+        return "I hope your day gets better"
+
+
+
+
+print("Welcome to a day in my week bot")
+print("Please enter a day, Mon-Sun")
+
+user_response = ""
+#TODO: we want to keep repeating until the user enters "done" what should we put here?
+while True:
+user_response = input("How are you feeling today?: ")
+
+# Quits program when user responds with 'done'
+if user_response == 'done':
+    break
+
+
+bot_response = get_day_bot_response(user_response)
+print(bot_response)
+
