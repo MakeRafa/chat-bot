dcec7a3ebec8c1d159026298cc9d2de12a44077b
Rafa <rafavazquez229@gmail.com>
2020-09-22 23:30:04 -0700

    now im done gang

--- chat.py
+++ chat.py
@@ -1,20 +1,23 @@
-# This will give you access to the random module or library.
-# choice() will randomly return an element in a list.
-# Read more: https://pynative.com/python-random-choice/
 
 from random import choice
 
-#combine functions and conditionals to get a response from the bot
+
 def get_day_bot_response(user_response):
 
-#add some bot responses to this list
-#bot_response_happy = ["omg! great!", "Keep smiling!", "I love to see you happy!"]
+
+#bot_response_day = ["jibber jabber about said day"]
     bot_response_Monday = ["SPD class while barely awake", "Napping or gaming it"]
+
     bot_response_Tuesday = ["CS class and lunch ", "Web class till 6 then dinner"]
+
     bot_response_Wednesday = ["SPD class ", "Doing HW all day"]
+
     bot_response_Thursday = ["CS class and lunch ", "Web class till 6 then dinner"]
+
     bot_response_Friday = ["Sleeping in because no A.M. class ", "EC while eating dinner"]
+
     bot_response_Saturday = ["Saturdays are for the boys"]
+
     bot_response_Sunday = ["Waking up after 11am", "Doing all the HW that is due "]
 
 
@@ -46,18 +49,14 @@ def get_day_bot_response(user_response):
 
 
 print("Welcome to a day in my week bot")
-print("Please enter a day, Mon-Sun to find out what I am doing")
+print("Choose any day to get an insight of my schedule")
 
 user_response = ""
-#TODO: we want to keep repeating until the user enters "done" what should we put here?
+
 while True:
-user_response = input("How are you feeling today?: ")
+    user_response = input("Pick a day Monday-Sunday: ")
+    if user_response == "done":
+        break
 
-# Quits program when user responds with 'done'
-if user_response == 'done':
-    break
-
-
-bot_response = get_day_bot_response(user_response)
-print(bot_response)
-
+    bot_response = get_day_bot_response(user_response)
+    print(bot_response)
