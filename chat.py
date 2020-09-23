57971feabc0b6737e514c41a8a4e3ea73dbcbe11
Rafa <rafavazquez229@gmail.com>
2020-09-22 23:05:40 -0700

    if elif else done

--- chat.py
+++ chat.py
@@ -9,13 +9,29 @@ def get_day_bot_response(user_response):
 
 #add some bot responses to this list
 #bot_response_happy = ["omg! great!", "Keep smiling!", "I love to see you happy!"]
-bot_response_Monday = ["SPD class with the bois", "Sleeping in with the bois"]
+    bot_response_Monday = ["SPD class with the ", "Sleeping in "]
+    bot_response_Tuesday = ["CS class and lunch ", "Web class till 6 then dinner"]
 
     if user_response == "Monday":
         return choice(bot_response_Monday)
 
     elif user_response == "Tuesday":
         return choice(bot_response_Tuesday)
+
+    elif user_response == "Wednesday":
+        return choice(bot_response_Wednesday)
+
+    elif user_response == "Thursday":
+        return choice(bot_response_Thursday)
+
+    elif user_response == "Friday":
+        return choice(bot_response_Friday)
+
+    elif user_response == "Saturday":
+        return choice(bot_response_Saturday)
+
+    elif user_response == "Sunday":
+        return choice(bot_response_Sunday)
 
     else:
         return "I hope your day gets better"
@@ -24,7 +40,7 @@ bot_response_Monday = ["SPD class with the bois", "Sleeping in with the bois"]
 
 
 print("Welcome to a day in my week bot")
-print("Please enter a day, Mon-Sun")
+print("Please enter a day, Mon-Sun to find out what I am doing")
 
 user_response = ""
 #TODO: we want to keep repeating until the user enters "done" what should we put here?
